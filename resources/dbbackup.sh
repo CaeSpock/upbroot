0f777b712129bd3d053833108a44e01a648aa88f
Carlos Anibarro <32886115+CaeSpock@users.noreply.github.com>
2018-03-22 22:15:04 -0400

    Delete dbbackup.sh

--- resources/dbbackup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo Generating backup of the database
-mysqldump -u'upbroot' -p'upb2018r00t!' --complete-insert=true --extended-insert=false upbroot > /home/upbroot/resources/upbroot.sql
