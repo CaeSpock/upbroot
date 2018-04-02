37e3bda23eb9d3481d30f5121594206128abd84f
Carlos Anibarro <32886115+CaeSpock@users.noreply.github.com>
2018-04-02 18:59:21 -0400

    Added new pid for mailaliases process

--- processes/paths.inc.php
+++ processes/paths.inc.php
@@ -1,8 +1,10 @@
 <?php
   // Please configure all the paths needed for "runtasks.php"
-   $pathinc    = "/home/upbroot/public_html/v1/inc";
-   $pathlogops = "/home/upbroot/logs/operations";
-   $pathlogsql = "/home/upbroot/logs/sql";
-  $lockfilepid = "/home/upbroot/processes/lock.pid";
+    $pathinc    = "/home/upbroot/public_html/v1/inc";
+    $pathlogops = "/home/upbroot/logs/operations";
+    $pathlogsql = "/home/upbroot/logs/sql";
+   $lockfilepid = "/home/upbroot/processes/lock.pid";
+  $lockaliaspid = "/home/upbroot/processes/alias.pid";
+ 
   // This include uses the full path to the file, as its loated outside of the web directory
   include_once("/home/upbroot/conf/conf.inc.php");
